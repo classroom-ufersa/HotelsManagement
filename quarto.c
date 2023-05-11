62abe97421f4be005753b30335f18d1f192c0d2f
Joana Oliveira <97909025+JoanaLOliveira@users.noreply.github.com>
2023-05-11 13:27:29 -0300

    fim

--- quarto.c
+++ quarto.c
@@ -7,7 +7,7 @@
 struct quarto{
     char tipo[20];
     float preco;
-    int disp;
+    char disp[3];
     int num;
     int qntd;
 };
@@ -22,24 +22,23 @@ Quarto* insereq(void){
 	printf("Informe quantos quartos deseja adicionar no hotel: ");
 	scanf("%d", &qntd);
 	Quarto* new = (Quarto*)malloc(qntd * sizeof(Quarto));
-	
 	for(i=0;i<qntd;i++){
 		printf("Tipo do quarto: ");
 		scanf("%s", new[i].tipo);
 		printf("Preco do quarto: ");
 		scanf("%f", &new[i].preco);
 		printf("Disponibilidade do quarto: ");
-		scanf("%d", &new[i].disp);
+		scanf("%s", new[i].disp);
 		printf("numero do quarto: ");
 		scanf("%d", &new[i].num);
 	}
 	FILE* arq_quarto = fopen("quarto.txt", "a"); 
 	if(arq_quarto == NULL){ 
-		 
 		printf("Erro ao abrir o arquivo: "); 
 		return 0; 
 	}
-	fprintf(arq_quarto,"tipo:%s\npreco:%f\ndisponibilidade:%d\nnumero:%d\n", new->tipo, new->preco, new->disp, new->num); //salvando os dados do usuario dentro do arquivo        
+	fprintf(arq_quarto,"tipo:%s\npreco:%f\ndisponibilidade:%s\nnumero:%d\n", new->tipo, new->preco, new->disp, new->num);       
+	printf("\nDados adicionados no arquivo!\n");
 	printf("\n");
 	fclose(arq_quarto); 
     return new;
@@ -54,34 +53,10 @@ int vaziaq(Listaq *list){
 	return (list==NULL);
 }
 
-Listaq* excluirq(Listaq* list){
-	int numq;
-	Listaq* ant = NULL;
-	Listaq* prox = list;
-	printf("Digite o numero do  que deseja excluir\n");
-    scanf("%d", &numq);
-	while ((prox->info->num)!= 0){
-		if(prox == NULL){
-			return list;
-		}
-		ant = prox;
-			prox = prox->next;
-	}
-	if(ant == NULL){
-		list = prox-> next;
-	}
-	else{
-		ant->next = prox-> next;
-	}
-	free(prox);
-	return list;
-}
-
-
 Listaq * buscarq( Listaq * list){
 	int nume;
 	Listaq * cont;
-	printf("Qual quarto deseja excluir:\n");
+	printf("Qual quarto deseja buscar:\n");
 	scanf("%d",&nume);
 	
 	for(cont = list; cont!=NULL; cont=cont->next){
@@ -89,7 +64,6 @@ Listaq * buscarq( Listaq * list){
 		    return cont;
 	    }
     }
-	return NULL ;
 }
 
 Listaq *editaq(Listaq *list){
@@ -101,7 +75,7 @@ Listaq *editaq(Listaq *list){
 	for(cont=list; cont!=NULL; cont=cont->next){
 		if(cont->info->num ==edit){				
 			printf("Qual a nova Disponibilidade: ");
-			scanf(" %d\n", &cont->info->disp);
+			scanf(" %s\n", cont->info->disp);
 			printf("Qual o Preco do quarto: ");
 			scanf(" %f\n", &cont->info->preco);
 			printf("Qual o Tipo do quarto: ");
@@ -110,31 +84,10 @@ Listaq *editaq(Listaq *list){
 	}
 	return cont;
 }
-	
-void quantitativoq(Listaq * list){
-    Listaq *percorre;
-	int  cont= 0;
-
-	for(percorre=list; percorre!=NULL; percorre = percorre -> next){
-      cont++;
-	}
-
-	printf("Quartidade de quarto e : %d\n", cont);
-
-}
-
-void listarq(Listaq *list){
-	Listaq *cont;
-
-	for(cont=list; cont!=NULL; cont=cont->next){
-		printf("|Tipo: %s |Numero: %d | Preco: %f | Disponibilidade: %d |\n", cont->info->tipo, cont->info->num, cont->info->preco, cont->info->disp);
-		printf("\n");
-	}
-}
 
 void quantitativa_quartos(Listah *list){
     Listah *percorre;
-    int cont;
+    int aux;
     char nome_hotel[50];
     printf("Digite o nome do hotel que deseja consultar: ");
     scanf("%s", nome_hotel);
@@ -142,9 +95,33 @@ void quantitativa_quartos(Listah *list){
     for(percorre=list; percorre!=NULL; percorre = percorre->next){
         if(strcmp(percorre->info->nome, nome_hotel) == 0){
             if(percorre->info->room!=NULL){
-                cont++;
+                aux++;
             }
         }
     }
-    printf("Quantitativa de quartos no hotel %s: %d", nome_hotel, cont);
+    printf("Quantitativa de quartos no hotel %s: %d", nome_hotel, aux);
+}
+
+void consultar_quartos(Listah *list){
+	Listah *percorre;
+    char nome_hotel[50];
+    printf("Digite o nome do hotel que deseja consultar: ");
+    scanf("%s", nome_hotel);
+	for(percorre=list; percorre!=NULL; percorre=percorre->next){
+		if(strcmp(percorre->info->nome, nome_hotel) == 0){
+            if((percorre->info->room->disp) == 'Sim' || (percorre->info->room->disp) =="sim"){
+                printf("Localidade: %s\n", percorre->info->local);
+                printf("Avaliacao: %s", percorre->info->avali);
+                printf("Numero do quarto: %d\n", percorre->info->room->num);
+                printf("Tipo do quarto: %s\n", percorre->info->room->tipo);
+                printf("Preco: %f\n", &percorre->info->room->preco);
+            }
+            else{
+                printf("Quarto indisponivel!\n");
+            }    
+		}
+        else{
+            printf("Hotel nao encontrado!\n");
+        }
+	}
 }
