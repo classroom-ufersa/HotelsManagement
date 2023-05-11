31d841b3bdf7b870963ebd7e78739ef78e08ce39
Joana Oliveira <97909025+JoanaLOliveira@users.noreply.github.com>
2023-05-11 13:28:06 -0300

    fim

--- quarto.h
+++ quarto.h
@@ -8,14 +8,10 @@ Listaq* lst_criaq(void);
 
 int vaziaq(Listaq *list);
 
-Listaq *excluirq(Listaq* list);
-
 Listaq *buscarq( Listaq * list);
 
 Listaq *editaq(Listaq *list);
 
-void quantitativoq(Listaq * list);
-
-void listarq(Listaq *list);
-
 void quantitativa_quartos(Listah *list);
+
+void consultar_quartos(Listah*list);
